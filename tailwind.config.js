8333456336a1341d874176b5b2589b19a1b27f14
Nick Sampsell <nick@nicksampsell.com>
2023-11-07 07:16:09 -0500

    Added images and colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./**/*.php"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      sage: '607f80',
+      brown: '463a2e',
+      coral: 'ea9e7a',
+      earthyGreen: '9a9b7a',
+      mutedBlueGreen: '56757a',
+      mutedGray: '7383a3',
+      warmBeige: 'c9b59d'
+    },
     extend: {},
   },
   plugins: [
@@ -9,3 +19,17 @@ module.exports = {
     require('@tailwindcss/typography')
   ],
 }
+
+/*
+Example scheme:
+Primary: coral
+Secondary: mutedBlueGreen
+Tertiary: brown
+Quaternary: mutedGray
+Accent: warmBeige
+
+Alternative:
+Primary: mutedBoueGreen
+Secondary: brown
+Accent: earthyGreen (or coral)
+*/
