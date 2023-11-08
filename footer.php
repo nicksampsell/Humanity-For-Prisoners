c20bc4b5407fd22727caca203745f806897a8f17
Nick Sampsell <nick@nicksampsell.com>
2023-11-08 18:27:15 -0500

    Work on header

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
 <?php wp_footer(); ?>
+<script>
+  document.write(
+    '<script src="http://' +
+      (location.host || '${1:localhost}').split(':')[0] +
+      ':${2:35729}/livereload.js?snipver=1"></' +
+      'script>'
+  );
+</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -9,12 +9,38 @@
     <script src="https://kit.fontawesome.com/d20cb14b94.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header class="flex flex-row justify-between items-center bg-mutedBlueGreen">
+    <header class="flex flex-row justify-between items-center shadow bg-gradient-to-b from-[#56757A] to-[#4d696d] border-b-4">
         <a href="/" id="logo">
             <span>Humanity for Prisoners</span>
-            <div class="bg-green-200 border rounded p-2 flex justify-between spacing-x-3">
-                <a href="#" class="flex flex-col justify-center text-center grow-1"><i class="fa-regular fa-heart bg-black w-fit p-5 rounded-full text-white text-3xl max-w-fit"></i><span>Support Our Work</span></a>
-                <a href="#" class="flex flex-col justify-center text-center grow-1"><i class="fa-solid fa-ticket bg-black w-fit p-5 rounded-full text-white text-3xl max-w-fit"></i><span>Purchase <em>Voices of Humanity</em> Tickets</span></a>
+        </a>
+            <div>
+                <ul class="socialMediaIcons flex flex-row space-x-3 my-2">
+                    <li><a href="https://www.facebook.com/humanityforprisoners/">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <span>Facebook</span>
+                    </a></li>
+                    <li><a href="https://twitter.com/humanity4prison">
+                        <i class="fa-brands fa-x-twitter"></i>
+                        <span>X (Twitter)</span>
+                    </a></li>
+                    <li><a href="https://www.instagram.com/humanityforprisoners/">
+                        <i class="fa-brands fa-instagram"></i>
+                        <span>Instagram</span>
+                    </a></li>
+                    <li><a href="https://www.youtube.com/user/humanity4prisoners">
+                        <i class="fa-brands fa-youtube"></i>
+                        <span>Youtube</span>
+                    </a></li>
+                    <li><a href="https://humanityforprisoners.blogspot.com/">
+                        <i class="fa-brands fa-blogger-b"></i>
+                        <span>Blogger</span>
+                    </a></li>
+                </ul>
+            <div class="bg-beigeLight border-t-4 border-l-4 border-beigeMedium rounded-tl-3xl p-5 flex justify-between spacing-x-3">
+                <a href="#" class="flex flex-col justify-start text-center grow-1 items-center content-center w-1/2 h-full"><i class="fa-regular fa-heart bg-black w-fit p-5 rounded-full text-white text-3xl max-w-fit mb-2"></i><span>Support Our Work</span></a>
+                <a href="#" class="flex flex-col justify-start text-center grow-1 items-center content-center w-1/2 h-full"><i class="fa-solid fa-ticket bg-black w-fit p-5 rounded-full text-white text-3xl max-w-fit mb-2"></i><span>Purchase <em>Voices of Humanity</em> Tickets</span></a>
+            </div>
             </div>
         </a>
     </header>
+ 
