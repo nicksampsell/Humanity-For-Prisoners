02d003d407db4e4dfd9213b8cc8d60e459deaa33
Nick Sampsell <nick@nicksampsell.com>
2023-11-09 06:39:56 -0500

    social media icons mostly in place. still need hover states

--- header.php
+++ header.php
@@ -14,31 +14,41 @@
             <span>Humanity for Prisoners</span>
         </a>
             <div>
-                <ul class="socialMediaIcons flex flex-row space-x-3 my-2">
-                    <li><a href="https://www.facebook.com/humanityforprisoners/">
-                        <i class="fa-brands fa-facebook-f"></i>
-                        <span>Facebook</span>
-                    </a></li>
-                    <li><a href="https://twitter.com/humanity4prison">
-                        <i class="fa-brands fa-x-twitter"></i>
-                        <span>X (Twitter)</span>
-                    </a></li>
-                    <li><a href="https://www.instagram.com/humanityforprisoners/">
-                        <i class="fa-brands fa-instagram"></i>
-                        <span>Instagram</span>
-                    </a></li>
-                    <li><a href="https://www.youtube.com/user/humanity4prisoners">
-                        <i class="fa-brands fa-youtube"></i>
-                        <span>Youtube</span>
-                    </a></li>
-                    <li><a href="https://humanityforprisoners.blogspot.com/">
-                        <i class="fa-brands fa-blogger-b"></i>
-                        <span>Blogger</span>
-                    </a></li>
+                <ul class="flex flex-row justify-end m-3 space-x-3 socialMediaIcons">
+                    <li class="rounded bg-[#4267B2] w-[40px] h-[40px] shadow">
+                        <a href="https://www.facebook.com/humanityforprisoners/" class="flex items-center justify-center w-full h-full text-white">
+                            <i class="text-2xl fa-brands fa-facebook-f"></i>
+                            <span>Facebook</span>
+                        </a>
+                    </li>
+                    <li class="rounded bg-[#000] w-[40px] h-[40px] shadow">
+                        <a href="https://twitter.com/humanity4prison" class="flex items-center justify-center w-full h-full text-white">
+                            <i class="text-2xl fa-brands fa-x-twitter"></i>
+                            <span>X (Twitter)</span>
+                        </a>
+                    </li>
+                    <li class="rounded bg-[#fb0074] w-[40px] h-[40px] shadow">
+                        <a href="https://www.instagram.com/humanityforprisoners/" class="flex items-center justify-center w-full h-full text-white">
+                            <i class="text-2xl fa-brands fa-instagram"></i>
+                            <span>Instagram</span>
+                        </a>
+                    </li>
+                    <li class="rounded bg-[#FF0000] w-[40px] h-[40px] shadow">
+                        <a href="https://www.youtube.com/user/humanity4prisoners" class="flex items-center justify-center w-full h-full text-white">
+                            <i class="text-2xl fa-brands fa-youtube"></i>
+                            <span>Youtube</span>
+                        </a>
+                    </li>
+                    <li class="rounded bg-[#f68221] w-[40px] h-[40px] shadow">
+                        <a href="https://humanityforprisoners.blogspot.com/" class="flex items-center justify-center w-full h-full text-white">
+                            <i class="text-2xl fa-brands fa-blogger-b"></i>
+                            <span>Blogger</span>
+                        </a>
+                    </li>
                 </ul>
-            <div class="bg-beigeLight border-t-4 border-l-4 border-beigeMedium rounded-tl-3xl p-5 flex justify-between spacing-x-3">
-                <a href="#" class="flex flex-col justify-start text-center grow-1 items-center content-center w-1/2 h-full"><i class="fa-regular fa-heart bg-black w-fit p-5 rounded-full text-white text-3xl max-w-fit mb-2"></i><span>Support Our Work</span></a>
-                <a href="#" class="flex flex-col justify-start text-center grow-1 items-center content-center w-1/2 h-full"><i class="fa-solid fa-ticket bg-black w-fit p-5 rounded-full text-white text-3xl max-w-fit mb-2"></i><span>Purchase <em>Voices of Humanity</em> Tickets</span></a>
+            <div class="flex justify-between p-5 border-t-4 border-l-4 bg-beigeLight border-beigeMedium rounded-tl-3xl spacing-x-3">
+                <a href="#" class="flex flex-col items-center content-center justify-start w-1/2 h-full text-center grow-1"><i class="p-5 mb-2 text-3xl text-white bg-black rounded-full fa-regular fa-heart w-fit max-w-fit"></i><span>Support Our Work</span></a>
+                <a href="#" class="flex flex-col items-center content-center justify-start w-1/2 h-full text-center grow-1"><i class="p-5 mb-2 text-3xl text-white bg-black rounded-full fa-solid fa-ticket w-fit max-w-fit"></i><span>Purchase <em>Voices of Humanity</em> Tickets</span></a>
             </div>
             </div>
         </a>
